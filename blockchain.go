4a1230cfecf4c61516982fd8540a9f2dc13df4a1
Elis Lulja <elulja@cisco.com>
2021-10-13 16:19:08 +0200

    Validate a chain and replace with new one
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const (
 	genesisBlockData string = "this is the genesis block!"
@@ -42,5 +45,39 @@ func (b *BlockChain) PushBlock(block Block) error {
 	// outside, but here everything is under the same main package.
 	// Move this to another pkg?
 	b.chain = append(b.chain, block)
+	return nil
+}
+
+// ValidateChain checks if the provided chain is correct and returns an
+// error if not.
+func ValidateChain(chain []Block) error {
+	if chain[0].Data != genesisBlockData {
+		// We are comparing the data inside of the genesis block.
+		// The probability of hashes being the same with different data inside
+		// is very very low, but I don't want to re-calculate the hash :)
+		// TODO: make the genesis block a const?
+		return fmt.Errorf("genesis block is wrong")
+	}
+
+	for i := 1; i < len(chain); i++ {
+		if err := ValidateBlock(chain[i], chain[i-1]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (b *BlockChain) ReplaceWith(newChain []Block) error {
+	// ValidateChain may take a while, so we better check the lens first
+	if len(newChain) <= len(b.chain) {
+		return fmt.Errorf("new chain is not longer than the current one")
+	}
+
+	if err := ValidateChain(newChain); err != nil {
+		return err
+	}
+
+	b.chain = newChain
 	return nil
 }
