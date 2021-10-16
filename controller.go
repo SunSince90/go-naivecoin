ac107c3097f5942254ffb9088b987480cb4b4a28
Elis Lulja <elulja@cisco.com>
2021-10-16 20:59:46 +0200

    Create PodEventHandler
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- controller.go
+++ controller.go
@@ -8,68 +8,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
-
-func handleCreateEvent(pod *corev1.Pod, myself string) {
-	l := log.With().Str("name", pod.Name).Str("ip", pod.Status.PodIP).Logger()
-	if pod.Name == myself {
-		l.Info().Msg("ignoring because it is me")
-		return
-	}
-
-	if val, exists := pod.Labels["app"]; !exists || val != "go-naivecoin" {
-		l.Info().Msg("ignoring because it is not related to go-naivecoin")
-		return
-	}
-
-	if pod.Status.Phase == corev1.PodRunning {
-		// TODO: ip or something else?
-		l.Info().Msg("found a new peer")
-
-		// TODO: peers should be a struct, so that we can establish a
-		// connection with this peer when it is added.
-		peers[pod.Name] = pod.Status.PodIP
-	}
-}
-
-func handleUpdateFunc(curr, prev *corev1.Pod, myself string) {
-	l := log.With().Str("name", curr.Name).Str("ip", curr.Status.PodIP).Logger()
-	if curr.Name == myself {
-		l.Info().Msg("ignoring because it is me")
-		return
-	}
-
-	if val, exists := curr.Labels["app"]; !exists || val != "go-naivecoin" {
-		l.Info().Msg("ignoring because it is not related to go-naivecoin")
-		return
-	}
-
-	if len(curr.Status.PodIP) == 0 {
-		l.Info().Msg("skipping: pod does not have an IP")
-		return
-	}
-
-	if curr.Status.Phase == prev.Status.Phase {
-		log.Info().Msg("same status as before")
-		return
-	}
-
-	if curr.Status.Phase == corev1.PodRunning {
-		l.Info().Msg("found a new running peer")
-		peers[curr.Name] = curr.Status.PodIP
-	} else {
-		l.Info().Msg("found a not running peer")
-		delete(peers, curr.Name)
-	}
-}
-
-func handleDeleteFunc(pod *corev1.Pod) {
-	delete(peers, pod.Name)
-}
 
 // GetControllerManager creates the pod controller and returns its manager so
 // that it could be started.
@@ -91,11 +33,120 @@ func GetControllerManager(namespace string) (manager.Manager, error) {
 	return mgr, nil
 }
 
+// PodEventHandler is a structure that handles event related to pods.
+type PodEventHandler struct {
+	myself string
+}
+
+// NewPodEventHandler returns a new PodEventHandler.
+func NewPodEventHandler(myself string) *PodEventHandler {
+	return &PodEventHandler{
+		myself: myself,
+	}
+}
+
+// Create handles pod Create events.
+func (p *PodEventHandler) Create(ce event.CreateEvent, _ workqueue.RateLimitingInterface) {
+	pod, ok := ce.Object.(*corev1.Pod)
+	if !ok {
+		// TODO: log this
+		log.Error().Str("event", "Create").Msg("skipping: could not successfully parse event")
+		return
+	}
+
+	l := log.With().Str("name", pod.Name).Str("ip", pod.Status.PodIP).Logger()
+	if pod.Name == p.myself {
+		l.Info().Msg("ignoring because it is me")
+		return
+	}
+
+	if val, exists := pod.Labels["app"]; !exists || val != "go-naivecoin" {
+		l.Info().Msg("ignoring because it is not related to go-naivecoin")
+		return
+	}
+
+	if pod.Status.Phase != corev1.PodRunning {
+		// TODO: only verbose
+		l.Info().Msg("ignoring because it is not related to go-naivecoin")
+		return
+	}
+
+	l.Info().Msg("found a new peer")
+
+	// TODO: send event
+}
+
+// Update handes Update events.
+func (p *PodEventHandler) Update(ue event.UpdateEvent, _ workqueue.RateLimitingInterface) {
+	currPod, currOk := ue.ObjectNew.(*corev1.Pod)
+	prevPod, prevOk := ue.ObjectOld.(*corev1.Pod)
+
+	if !currOk || !prevOk {
+		log.Error().Str("event", "Update").Msg("skipping: could not parse the events")
+		return
+	}
+
+	l := log.With().Str("name", currPod.Name).Str("ip", currPod.Status.PodIP).Logger()
+	if currPod.Name == p.myself {
+		l.Info().Msg("ignoring because it is me")
+		return
+	}
+
+	if val, exists := currPod.Labels["app"]; !exists || val != "go-naivecoin" {
+		l.Info().Msg("ignoring because it is not related to go-naivecoin")
+		return
+	}
+
+	if len(currPod.Status.PodIP) == 0 {
+		l.Info().Msg("skipping: pod does not have an IP")
+		return
+	}
+
+	if currPod.DeletionTimestamp != nil {
+		// This is the way to know when a resource is being deleted.
+		// TODO: delete the pod
+	}
+
+	if currPod.Status.Phase == prevPod.Status.Phase {
+		log.Info().Msg("same status as before")
+		return
+	}
+
+	if currPod.Status.Phase == corev1.PodRunning {
+		l.Info().Msg("found a new running peer")
+	} else {
+		l.Info().Msg("found a not running peer")
+	}
+
+	// TODO: add event
+}
+
+// Delete handles pod Delete events.
+func (p *PodEventHandler) Delete(de event.DeleteEvent, _ workqueue.RateLimitingInterface) {
+	pod, ok := de.Object.(*corev1.Pod)
+	if !ok {
+		// TODO: log this
+		log.Error().Str("event", "Delete").Msg("skipping: could not successfully parse event")
+		return
+	}
+
+	log.Info().Msg("found dead peer")
+
+	// TODO: add event
+	_ = pod
+}
+
+// Generic handles pod events that are neither Create, Update or Delete.
+func (p *PodEventHandler) Generic(ge event.GenericEvent, _ workqueue.RateLimitingInterface) {
+	log.Info().Msg("skipping unknown event")
+}
+
 // SetPodController sets the pod controller to the provided manager.
 //
 // The second parameter is the name of the running pod, so that the pod
 // controller can correctly avoid parsing events about itself.
-func SetPodController(mgr manager.Manager, myself string) (controller.Controller, error) {
+func SetPodController(mgr manager.Manager, podEH *PodEventHandler) (controller.Controller, error) {
+	// podController := NewPod
 	c, err := controller.New("pod-controller", mgr, controller.Options{
 		Reconciler: reconcile.Func(func(context.Context, reconcile.Request) (reconcile.Result, error) {
 			return reconcile.Result{}, nil
@@ -106,26 +157,7 @@ func SetPodController(mgr manager.Manager, myself string) (controller.Controller
 	}
 
 	// Watch for Pod create / update / delete events and call Reconcile
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.Funcs{
-		CreateFunc: func(ce event.CreateEvent, rli workqueue.RateLimitingInterface) {
-			if pod, ok := ce.Object.(*corev1.Pod); ok {
-				handleCreateEvent(pod, myself)
-			}
-		},
-		UpdateFunc: func(ue event.UpdateEvent, rli workqueue.RateLimitingInterface) {
-			newPod, newOK := ue.ObjectNew.(*corev1.Pod)
-			oldPod, oldOK := ue.ObjectOld.(*corev1.Pod)
-
-			if newOK && oldOK {
-				handleUpdateFunc(newPod, oldPod, myself)
-			}
-		},
-		DeleteFunc: func(de event.DeleteEvent, rli workqueue.RateLimitingInterface) {
-			if pod, ok := de.Object.(*corev1.Pod); ok {
-				handleDeleteFunc(pod)
-			}
-		},
-	})
+	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, podEH)
 	if err != nil {
 		return nil, err
 	}
